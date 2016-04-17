cb3d0960af712633c9e96263d60bd080a2c8743e
Alice Dourado <alice.dourado1@gmail.com>
2016-04-17 19:32:16 +0100

    Hey

--- main.cpp
+++ main.cpp
@@ -3,8 +3,27 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <limits.h>
 
 #define INFINITE -1
+
+
+bool less_or_equal_infinity(int a, int b){
+	if(a == INFINITE)
+		return false;
+	if(b == INFINITE)
+		return true;
+	return a <= b;
+} 
+
+bool less_infinity(int a, int b){
+	if(a == INFINITE)
+		return false;
+	if(b == INFINITE)
+		return true;
+	return a < b;
+} 
 
 using namespace std;
 
@@ -19,6 +38,7 @@ public:
 	}
 };
 
+
 class DirectedGraph{
 private:
 	int _nverts;
@@ -26,6 +46,14 @@ private:
 	int _nafls;
 	int* _afls; // Array with all the identifiers for afilliate points
 	list<Edge>* _adjLists;
+
+	/* Aux variables for Bellman-Ford */
+	vector<int> _h;
+	bool _bfChanges;
+
+	/* Aux variables for Dijkstra */
+	vector<vector<int> > _dijkstraMaster;
+
 
 public:
 	DirectedGraph(int nverts, int nedges, int nafls, int* afls){
@@ -35,6 +63,9 @@ public:
 		_nafls = nafls;
 		_afls = afls;
 		_adjLists = new list<Edge>[nverts];
+		_h = vector<int>(_nverts, 0);
+		_bfChanges = true;
+		_dijkstraMaster = vector< vector<int> >(_nafls, vector<int>(_nverts, INFINITE));
 	}
 
 	~DirectedGraph(){
@@ -53,6 +84,81 @@ public:
 		_adjLists[u].push_back(Edge(v, w));
 	}
 
+	void BellmanFord(){
+		for(int i = 0; i < _nverts; i++){
+			list<Edge>::iterator e;
+			for(e = _adjLists[i].begin(); e != _adjLists[i].end(); e++){
+				if((_h[i] + e->w) < _h[e->v]){
+					_h[e->v] = (_h[i] + e->w);
+					BellmanFord();
+				}
+			}
+		}
+	}
+
+	int newWeight(int u, Edge e){
+		return e.w + _h[u] - _h[e.v];
+	}
+
+	void Dijkstra(int act, int ind){
+		vector<bool> visited = vector<bool>(_nverts, false);
+		_dijkstraMaster[ind][act] = 0;
+
+		for(int i = 0; i < _nverts; i++){
+			int min=INFINITE, min_index = -1;
+			bool changed = false;
+			for(int j = 0; j < _nverts; j++){
+				
+				if((less_infinity(_dijkstraMaster[ind][j], min) && !visited[j])/* && _dijkstraMaster[act][j] != INFINITE */){
+					min = _dijkstraMaster[ind][j];
+					
+					min_index = j;
+					changed = true;
+				}
+			} 
+
+			if(!changed)
+				continue;
+
+			list<Edge>::iterator e;
+			for(e = _adjLists[min_index].begin(); e != _adjLists[min_index].end(); e++){
+				if(less_infinity((min + newWeight(min_index, *e)), _dijkstraMaster[ind][e->v])/* || _dijkstraMaster[act][e->v] == INFINITE */){
+					_dijkstraMaster[ind][e->v] = (min + newWeight(min_index, *e));
+				}
+			}
+
+			visited[min_index] = true;
+		}
+	
+	}
+
+	void Johnson(){
+		int min = INFINITE, cand, pt = INFINITE;
+		BellmanFord();
+		for(int i = 0; i < _nafls; i++){
+			//printGraph();
+			//printf("dijkstra\n");
+			Dijkstra(_afls[i], i);
+		}
+		for(int v = 0; v < _nverts; v++){
+			cand=0;
+			for(int u = 0; u < _nafls; u++){
+				if(_dijkstraMaster[u][v] == INFINITE)
+					break;
+				cand += _dijkstraMaster[u][v] + _h[v] - _h[_afls[u]];
+			}
+			printf("PONTO %d %d\n",v+1, cand );
+			if(less_infinity(cand, min)){
+				min=cand;
+				pt = v;
+			} 
+			if(cand == min)
+				pt = less_infinity(v, pt) ? v : pt;	
+		}
+
+	}
+
+
 	void printGraph(){
 		printf("Directed graph with %d vertices, %d edges and %d afilliates:\n", _nverts, _nedges, _nafls);
 		for(int i = 0; i < _nafls; i++){
@@ -65,24 +171,25 @@ public:
 		}
 		printf("\nEdges (orig: (dest, weight), ...):");
 		for(int i = 0; i < _nverts; i++){
-			printf("\n%d: ", i+1);
-			for(int j = 0; j < _adjLists[i].size(); j++){
+			printf("\n%d (h = %d, dij1=%d, dij2=%d): ", i+1, _h[i], _dijkstraMaster[0][i], _dijkstraMaster[1][i]);
+			for(int j = 0; j < (int)_adjLists[i].size(); j++){
 				std::list<Edge>::iterator it = std::next(_adjLists[i].begin(), j);
-				if(j == _adjLists[i].size() - 1)
-					printf("(%d, %d)", it->v, it->w);
+				if(j == (int)_adjLists[i].size() - 1)
+					printf("(%d, %d)", (it->v)+1, it->w);
 				else
-					printf("(%d, %d), ", it->v, it->w);
+					printf("(%d, %d), ", (it->v)+1, it->w);
 			}
 		}
 		printf("\n");
 	}
+
 };
 
 int main(){
 	int n_towns, n_afls, n_conns;
-	int *afls = new int[n_afls]; 
 
 	scanf("%d %d %d", &n_towns, &n_afls, &n_conns);
+	int *afls = new int[n_afls]; 
 	for(int i = 0; i < n_afls; i++){
 		scanf("%d", &(afls[i]));
 		afls[i]--; // Translating indexes - internally we start at zero
@@ -92,9 +199,10 @@ int main(){
 	for(int i = 0; i < n_conns; i++){
 		int u, v, w;
 		scanf("%d %d %d", &u, &v, &w);
-		g.addEdge(u, v, w);
+		g.addEdge(u-1, v-1, w); // Translating to internal index
 	}
 
+	g.Johnson();
 	g.printGraph();
 
 	// TRANSLATE INDEXES BACK FROM n-1 to n!!
